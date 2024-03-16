b3086d8eebdc25142b28363e2b68f1547c7e8405
Mamta9871 <mamtapal9871@gmail.com>
2024-03-16 19:02:31 +0530

    Implementing the cart Itemns page

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import {useContext} from 'react'
+import CartContext from '../store/cart-context';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext)
+  const numOfCartItems = cartCtx.items.reduce((curNum,item)=>{
+    return curNum + item.amount
+  },0)
+
   return (
     <div>
         <button className={classes.CartStyle} onClick={props.onClickCart}>
             <div>
             <ShoppingCartIcon id="Icon" fontSize='large'/> 
             </div>
-            <h1 className={classes.cart}> Your Cart <span>0</span></h1> 
-            
+            <h1 className={classes.cart}>Your Cart</h1> 
+            <span>{numOfCartItems}</span>
       </button>
     </div>
   )
